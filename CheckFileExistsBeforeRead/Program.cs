c097fe189df9c154378fe9e1daf788fb739140e5
leanhtai01 <leanhtai01@gmail.com>
2020-09-24 16:59:30 +0700

    Add project CheckFileExistsBeforeRead

---
+++ CheckFileExistsBeforeRead/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace CheckFileExistsBeforeRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "myfile.txt";
+
+            if (File.Exists(path))
+            {
+                using (var sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+
+                    sr.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not exists!");
+            }
+        }
+    }
+}
