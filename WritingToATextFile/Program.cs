67c7f4fc4d8f03c4a1a757623233c2a150150419
leanhtai01 <leanhtai01@gmail.com>
2020-09-24 17:08:11 +0700

    Add project WritingToATextFile

---
+++ WritingToATextFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace WritingToATextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "myfile.txt";
+
+            using (var sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine("ABC");
+                sw.WriteLine("DEF");
+                sw.Close();
+            }
+
+            // Reading from a file
+            if (File.Exists(path))
+            {
+                using (var sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+
+                    sr.Close();
+                }
+            }
+        }
+    }
+}
