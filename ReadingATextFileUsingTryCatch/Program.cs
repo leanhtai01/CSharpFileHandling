c20868f4414fd17f5476f034887f14d309cec2db
leanhtai01 <leanhtai01@gmail.com>
2020-09-24 16:46:05 +0700

    Add project ReadingATextFileUsingTryCatch

---
+++ ReadingATextFileUsingTryCatch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingATextFileUsingTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "myfile.txt";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+
+                    sr.Close();
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
